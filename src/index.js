02a14b7cf4352f90cb507e1d8184285ec5f4f656
Rodrigo Silva <rodrigob_silva@hotmail.com>
2023-05-25 10:50:49 -0400

    Adjust links

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <HomePage />
     </BrowserRouter>
   </React.StrictMode>
 );
